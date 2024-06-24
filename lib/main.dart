1e19a0d262113396c0433d8f06d57fed07bfb126
LUCIMARA <lucial85@gmail.com>
2024-06-24 16:08:18 -0300

    Versão Inicial com Criação de modelo de transações

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+main () => runApp(Expenses()); //inicia com o widget Expenses
+
+class Expenses extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage()
+    );
+  }
+}
+
+//cria a classe MyHomePage que é um widget da classe de home
+class MyHomePage extends StatelessWidget{
+
+  final _transactions = [
+    {
+      'id': 't1',
+      'title': 'Novo Tênis de Corrida',
+      'amount': 310.76,
+      'date': DateTime.now(),
+    },
+    {
+      'id': 't2',
+      'title': 'Conta de Luz',
+      'amount': 211.30,
+      'date': DateTime.now(),
+    }
+  ];
+
+
+
+  @override
+  Widget build(BuildContext contexrt) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+          )
+        ],
+      ),
+    );
+  }
+}
